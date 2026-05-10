1cbce41836c14c7df964cface21c99c7b7d9bd7f
Salma Majri <salmamajri7@gmail.com>
2026-05-10 11:33:27 +0100

    refactor: extract customer HTTP requests into a reusable Customer Service

--- src/app/customers/customers.ts
+++ src/app/customers/customers.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
+import { Customer } from '../services/customer';
 
 @Component({
   selector: 'app-customers',
@@ -11,16 +11,17 @@ import { CommonModule } from '@angular/common';
 })
 export class Customers implements OnInit {
   customers: any;
-  constructor(private http: HttpClient, private cdr: ChangeDetectorRef) {}
+
+  constructor(private customerService: Customer, private cdr: ChangeDetectorRef) {}
 
   ngOnInit(): void {
-    this.http.get("http://localhost:8085/customers").subscribe({
-      next: (data) => {
+    this.customerService.getCustomers().subscribe({
+      next: (data: any) => {
         this.customers = data;
-        console.log("Data received:", data);
+        console.log("Data received via Service:", data);
         this.cdr.detectChanges();
       },
-      error: (err) => {
+      error: (err: any) => {
         console.error("Error fetching customers:", err);
       }
     });
