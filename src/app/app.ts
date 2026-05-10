c83015a3927a4aacf4dfd36435b5234dc96b9ab1
Salma Majri <salmamajri7@gmail.com>
2026-05-10 10:59:49 +0100

    feat: setup standalone routing, http client and customers list component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
+import { RouterOutlet} from '@angular/router';
 import { Navbar } from './navbar/navbar';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [Navbar],
+  imports: [RouterOutlet, Navbar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/app/customers/customers.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-customers',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './customers.html',
+  styleUrl: './customers.css'
+})
+export class Customers implements OnInit {
+ customers: any;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get("http://localhost:8085/customers").subscribe({
+      next: (data) => {
+        this.customers = data;
+        console.log("Data received:", data);
+      },
+
+      error: (err) => {
+        console.error("Error fetching customers:", err);
+      }
+    });
+  }
+}
