12b7b31ab332a5ae9f5665f656c35d951cbbf5f1
Salma Majri <salmamajri7@gmail.com>
2026-05-10 11:06:32 +0100

    feat: fix change detection on customers list and update routes

--- src/app/customers/customers.ts
+++ src/app/customers/customers.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 
@@ -10,17 +10,16 @@ import { CommonModule } from '@angular/common';
   styleUrl: './customers.css'
 })
 export class Customers implements OnInit {
- customers: any;
-
-  constructor(private http: HttpClient) {}
+  customers: any;
+  constructor(private http: HttpClient, private cdr: ChangeDetectorRef) {}
 
   ngOnInit(): void {
     this.http.get("http://localhost:8085/customers").subscribe({
       next: (data) => {
         this.customers = data;
         console.log("Data received:", data);
+        this.cdr.detectChanges();
       },
-
       error: (err) => {
         console.error("Error fetching customers:", err);
       }
